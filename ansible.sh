3e1e405e17c7409173b4133ac5a9fa3c16704b0e
martin.goerz <martin.goerz@rwth-aachen.de>
2021-10-10 20:32:40 +0200

    make it an install script

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# find appropriate package manager
 declare -A osInfo;
 osInfo[/etc/debian_version]="apt-get install -y"
 osInfo[/etc/alpine-release]="apk --update add"
@@ -12,9 +14,19 @@ do
     fi
 done
 
-package="ansible"
+package="ansible git"
 
 sudo ${package_manager} ${package}
 
+# ensure git repo
+mkdir -p $HOME/dev/
+git clone https://gitlab.com/martin.goerz/ansible-desktop.gitlab $HOME/dev/ansible-desktop/
+
+# change to working dir
+cd $HOME/dev/ansible-desktop/
+
+# make sure requirements are installed
 sudo ansible-galaxy install -r requirements.yml
+
+# run ansible
 sudo ansible-playbook local.yml 
