50161db1a68e321f38417d9b6198a44fcb1c99df
martin.goerz <martin.goerz@rwth-aachen.de>
2021-10-10 20:25:21 +0200

    installing ansible before running the script

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+declare -A osInfo;
+osInfo[/etc/debian_version]="apt-get install -y"
+osInfo[/etc/alpine-release]="apk --update add"
+osInfo[/etc/centos-release]="yum install -y"
+osInfo[/etc/fedora-release]="dnf install -y"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+package="ansible"
+
+sudo ${package_manager} ${package}
+
 sudo ansible-galaxy install -r requirements.yml
 sudo ansible-playbook local.yml 
